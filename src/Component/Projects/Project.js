2a41bd1bba81f11e1856d8c82d6cbd62c355c1fd
soumya sambit mangaraj <smangaraj28@gmail.com>
2022-06-28 09:26:53 +0530

    project structure changes

--- src/Component/Projects/Project.js
+++ src/Component/Projects/Project.js
@@ -7,7 +7,9 @@ import {
 } from "react-icons/md";
 import React from 'react';
 import { FiExternalLink } from "react-icons/fi";
-export default () => {
+export default (props) => {
+  console.log(props);
+
   return (
     <>
       <section className="box-center-row" id="Projects">
@@ -20,47 +22,43 @@ export default () => {
         </div>
         <strong> Mega Project</strong> <hr />
         <div className="megaprojects">
-          <div className="grid-item">
-            <div className="Cards">
-              <div className="Card-header">
-                {/* <div className="card-avatar" /> */}
-
-                <h2>Universal Hotel Solution</h2>
-
-                <h2>
-                  {" "}
-                  <MdOutlineLabel className="icon" />
-                  {"\t"} final year Project
-                </h2>
-              </div>
-
-              <h2 className="content">
-                Developed this project using Angular8 & Python Flask Framework
-                with PostgreSQL having Travis CI Pipeline with Docker
-                containerization for providing management solutions from small
-                to large scale hotels. Implemented 3 way databinding using
-                Angular & Firebase. Architected Authentication for web
-                Application using OAuth2 and JSON web tokens (JWT).Wrote Testing
-                Suite to validate each UI level operation.
-              </h2>
-              <div className="bottom-button-export">
-                {" "}
-                <div className="text">
-                  <b>Lang - React native</b>
-                </div>
-                <a
-                  className="explorebtn"
-                  href="https://docs.google.com/document/d/1ceKJlsidtzayzkLIDJIm-9Jag0rtBMr205f-gP-1cO8/edit?usp=sharing"
-                >
-                  <div className="text">
-                    <h2>explore</h2>
-                    <FiExternalLink className="icon" />
+          {props?.mega.map((item) => (
+              <div className="grid-item">
+                <div className="Cards">
+                  <div className="Card-header">
+                    {/* <div className="card-avatar" /> */}
+    
+                    <h2>{item.title}</h2>
+    
+                    <h2>
+                      {" "}
+                      <MdOutlineLabel className="icon" />
+                      {"\t"} {item.year}
+                    </h2>
                   </div>
-                </a>
-              </div>
-              <br />
+    
+                  <h2 className="content">
+                    {item.description}
+                  </h2>
+                  <div className="bottom-button-export">
+                    {" "}
+                    <div className="text">
+                      <b>{item.skills}</b>
+                    </div>
+                    <a
+                      className="explorebtn"
+                      href={item.links}
+                    >
+                      <div className="text">
+                        <h2>explore</h2>
+                        <FiExternalLink className="icon" />
+                      </div>
+                    </a>
+                  </div>
+                  <br />
+                </div>
             </div>
-          </div>
+          ))}
         </div>
         <div className="sub-head">
           <strong> Mini Project</strong> <hr />
@@ -69,15 +67,7 @@ export default () => {
 
       <div className="Skills">
         <div className="grid-container">
-          {[
-            {
-              Title: "Personal Portfolio",
-              link: "https://gksjfg.csb.app/",
-              Lang: "React js",
-              content:
-                "Personal information about myself , just like different projects information about resuming and more.."
-            }
-          ].map((i) => (
+          {props?.mini.map((item) => (
             <div className="grid-item">
               <div className="Cards">
                 <div className="Card-header">
@@ -85,16 +75,16 @@ export default () => {
                    
                   </div> */}
                   <div>
-                    <h2> {i.Title}</h2>
+                    <h2> {item.title}</h2>
                   </div>
                 </div>
-                <h2 className="content">{i.content}</h2>
+                <h2 className="content">{item.description}</h2>
                 <div className="bottom-button-export">
                   {" "}
                   <div className="text">
-                    <b>React Js</b>
+                    <b>{item.skills}</b>
                   </div>
-                  <a href={i.link} className="explorebtn">
+                  <a href={item.links} className="explorebtn">
                     <div className="text ">
                       <h2>explore</h2>
                       <FiExternalLink className="icon" />
