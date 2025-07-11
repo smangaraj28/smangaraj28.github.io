4551303cf65058409f78e2e10c0f5d39ab274d44
ssmangar <soumya.mangaraj@oracle.com>
2025-07-12 01:32:18 +0530

    refactoring

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel"; // Image Slider library
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Carousel Styles
-import img1 from "../../assets/034.png";
-import img2 from "../../assets/about.png";
-import img3 from "../../assets/036.png";
+// import React from "react";
+// import { Carousel } from "react-responsive-carousel"; // Image Slider library
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // Carousel Styles
+// import img1 from "../../assets/034.png";
+// import img2 from "../../assets/about.png";
+// import img3 from "../../assets/036.png";
 
-const Gallery = () => {
-  const images = [
-    { src: img1, alt: "Image 1", text: "First engraving sentence." },
-    { src: img2, alt: "Image 2", text: "Second engraving sentence." },
-    { src: img3, alt: "Image 3", text: "Third engraving sentence." },
-  ];
+// const Gallery = () => {
+//   const images = [
+//     { src: img1, alt: "Image 1", text: "First engraving sentence." },
+//     { src: img2, alt: "Image 2", text: "Second engraving sentence." },
+//     { src: img3, alt: "Image 3", text: "Third engraving sentence." },
+//   ];
 
-  return (
-    <div className="gallery-container">
-      <div className="gallery">
-        <h1>Gallery</h1>
-        <Carousel>
-          {images.map((image, index) => (
-            <div key={index}>
-              <img className="carousel-image" src={image.src} alt={image.alt} />
-              <p className="legend">{image.text}</p> {/* Engraving text */}
-            </div>
-          ))}
-        </Carousel>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="gallery-container">
+//       <div className="gallery">
+//         <h1>Gallery</h1>
+//         <Carousel>
+//           {images.map((image, index) => (
+//             <div key={index}>
+//               <img className="carousel-image" src={image.src} alt={image.alt} />
+//               <p className="legend">{image.text}</p> {/* Engraving text */}
+//             </div>
+//           ))}
+//         </Carousel>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Gallery;
+// export default Gallery;
